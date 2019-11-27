104a89270c86ec95488eb19c64025b35657a2441
xrface <chlrlwns0923@gmail.com>
2019-11-27 21:13:36 +0900

    1127작업내용

---
+++ 1115/Assets/Scripts/AnnouncementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnouncementScript : MonoBehaviour
+{
+
+    public Transform user_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var userscript = user_transform.GetComponent<User>();
+
+        if(userscript.is_start == false)
+        {
+            Debug.Log("캐릭터를 생성하세요!!");  
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 1115/Assets/Scripts/User.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class User : MonoBehaviour
+{
+    public GameObject[] plant_list = new GameObject[5];
+    private int plant_cnt = 1;
+    public bool is_start = false;
+    
+
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
